8fc52771476cc7463524896f2cc247c818949ecc
Werton <mr.werton@gmail.com>
2023-07-23 00:49:57 -0300

    started

---
+++ lib/modules/auth/entity/user.dart
@@ -0,0 +1,75 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String _name;
+  final String _id;
+  final String _email;
+  final String _photo;
+  final bool _isAdmin;
+  final bool _isFono;
+
+  const User._({
+    required String id,
+    required String name,
+    required String email,
+    required String photo,
+    required bool isAdmin,
+    required bool isFono,
+  })  :_id = id,
+        _name = name,
+        _email = email,
+        _photo = photo,
+        _isAdmin = isAdmin,
+        _isFono = isFono;
+
+  String get id => _id;
+  String get name => _name;
+  String get email => _email;
+  String get photo => _photo;
+  bool get isAdmin => _isAdmin;
+  bool get isFono => _isFono;
+
+  factory User.create({
+    required String id,
+    required String name,
+    required String email,
+    required String photo,
+    required bool isFono,
+  }) {
+    return User._(
+      id: id,
+      name: name,
+      email: email,
+      photo: photo,
+      isAdmin: false,
+      isFono: isFono,
+    );
+  }
+
+  factory User.fromJson(Map<String, dynamic> map) {
+    return User._(
+      id: map['id'],
+      name: map['name'],
+      email: map['email'],
+      photo: map['photo'],
+      isAdmin: map['isAdmin'] ?? false,
+      isFono: map['isFono'] ?? false,
+    );
+  }
+
+  @override
+  List<Object?> get props => [_id, _name, _email, _photo, _isAdmin, _isFono];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': _id,
+      'name': _name,
+      'email': _email,
+      'photo': _photo,
+      'isAdmin': _isAdmin,
+      'isFono': _isFono,
+    };
+  }
+
+
+}
---
+++ test/modules/auth/entity/user_test.dart
@@ -0,0 +1,75 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testing_app/modules/auth/entity/user.dart';
+
+void main(){
+  late final String ID;
+  late final String NAME;
+  late final String EMAIL;
+  late final String PHOTO;
+  late final bool ISFONO;
+  late final bool ISADMIN;
+
+  setUpAll(() {
+     ID = faker.guid.guid();
+     NAME = faker.person.firstName();
+     EMAIL = faker.internet.email();
+     PHOTO = faker.internet.httpsUrl();
+     ISFONO = faker.randomGenerator.boolean();
+     ISADMIN = faker.randomGenerator.boolean();
+
+  });
+  group('User entity', () {
+    test("should be initialized with correctly params", () {
+
+    final sut = User.create(id: ID, name: NAME, email: EMAIL, photo: PHOTO, isFono: ISFONO);
+
+    expect(NAME, sut.name);
+    expect(EMAIL, sut.email);
+    expect(PHOTO, sut.photo);
+    expect(ISFONO, sut.isFono);
+    expect(false, sut.isAdmin);
+    });
+  });
+
+  test("should parse of json to model correctly", () {
+    Map<String, dynamic> jsonData = {
+      'id': ID,
+      'name': NAME,
+      'email': EMAIL,
+      'photo': PHOTO,
+      'isAdmin': ISADMIN,
+      'isFono': ISFONO,
+    };
+
+    User user = User.fromJson(jsonData);
+
+    expect(ID, user.id);
+    expect(NAME, user.name);
+    expect(EMAIL, user.email);
+    expect(PHOTO, user.photo);
+    expect(ISFONO, user.isFono);
+    expect(false, user.isAdmin);
+  });
+
+  test("should parse of model to json correctly", () {
+    Map<String, dynamic> JSONMOCK = {
+      'id': ID,
+      'name': NAME,
+      'email': EMAIL,
+      'photo': PHOTO,
+      'isAdmin': ISADMIN,
+      'isFono': ISFONO,
+    };
+    final user = User.create(id: ID, name: NAME, email: EMAIL, photo: PHOTO, isFono: ISFONO);
+    final sut = user.toJson();
+
+    expect(JSONMOCK['id'], user.id);
+    expect(JSONMOCK['name'], user.name);
+    expect(JSONMOCK['email'], user.email);
+    expect(JSONMOCK['photo'], user.photo);
+    expect(JSONMOCK['isFono'], user.isFono);
+    expect(JSONMOCK['isAdmin'], user.isAdmin);
+    expect(JSONMOCK, sut);
+  });
+}
